5e0d83961a7a3eed54854b3b34d651de22f6bea2
thmmsn <thommesen@me.com>
2022-08-24 15:30:18 +0200

    v3

---
+++ kana/precache-manifest.48c1f130add42017f43b0875ab2efa9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "89889688147bd7575d6327160d64e760.svg"
+  },
+  {
+    "url": "e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "url": "f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "revision": "c4d02c2b5bb54faa4b41fe8d4b2fd068",
+    "url": "index.html"
+  },
+  {
+    "url": "main.e1d0287bb82781aad3a0.js"
+  }
+]);
---
+++ kana/sw.js
@@ -0,0 +1,5 @@
+importScripts("precache-manifest.48c1f130add42017f43b0875ab2efa9c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
