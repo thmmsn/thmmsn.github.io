8c4595369fabbb00ba432c9a803ede6d43dc4c00
thmmsn <thommesen@me.com>
2022-08-25 17:50:37 +0200

    v2

---
+++ kanji/precache-manifest.c3ead38f333008cdbf651961a7bb9676.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "89889688147bd7575d6327160d64e760.svg"
+  },
+  {
+    "url": "e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "url": "f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "revision": "c3feac763bf17059b4de30a8beaa4b93",
+    "url": "index.html"
+  },
+  {
+    "url": "main.62bf45fbfc99504a5111.js"
+  }
+]);
---
+++ kanji/sw.js
@@ -0,0 +1,5 @@
+importScripts("precache-manifest.c3ead38f333008cdbf651961a7bb9676.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
