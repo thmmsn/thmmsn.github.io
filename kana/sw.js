21ab48d57b14b01f7a70161591275ecfd82df81c
thmmsn <thommesen@me.com>
2022-08-27 11:56:37 +0200

    del

--- kana/sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("precache-manifest.ed49ada48ccc2c9bdc04aaec2ea13ffd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-
